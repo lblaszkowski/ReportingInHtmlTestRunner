27b064eac7b81c61dd7d077b02f0bac8550e8678
lblaszkowski <lblaszkowski@onet.pl>
2019-07-04 16:23:27 +0200

    Adds img, drivers and file Upload img

---
+++ PageObjectModel/Test/Upload_image.py
@@ -0,0 +1,39 @@
+import unittest
+from selenium import webdriver
+from time import sleep
+
+
+url = 'https://www.tineye.com/'
+
+class UploadImagePage(unittest.TestCase):
+
+
+    def setUp(self, browser="mozilla"):
+        if browser == "chrome":
+            self.driver = webdriver.Chrome(executable_path=r'../Drivers/ChromeDrive_75/chromedriver.exe')
+            self.driver.maximize_window()
+            self.driver.get(url)
+        elif browser == "mozilla":
+            self.driver = webdriver.Firefox(executable_path=r'../Drivers/FirefoxDrive_24/geckodriver.exe')
+            self.driver.maximize_window()
+            self.driver.get(url)
+        else:
+            print("Brak przeglądarki")
+            raise Exception("Brak przeglądarki")
+        return self.driver
+
+    def tearDown(self):
+        self.driver.close()
+        self.driver.quit()
+
+    def test_UploadImagePage(self):
+        driver = self.driver
+        button_image = driver.find_element_by_id("upload-button")
+        button_image.click()
+        upload_image = driver.find_element_by_id("upload_box")
+        upload_image.send_keys('C:\\ReportingInHtmlTestRunner\\PageObjectModel\\image\\surfer-2335088_960_720.jpg')
+        sleep(5)
+
+
+
+
