acb07feb308d71d00bd747f72baab7c2b6fb407f
lblaszkowski <lblaszkowski@onet.pl>
2019-07-04 17:02:21 +0200

    Add Reporting Html Test Runner

--- PageObjectModel/Test/Upload_image.py
+++ PageObjectModel/Test/Upload_image.py
@@ -1,13 +1,14 @@
 import unittest
 from selenium import webdriver
 from time import sleep
+import HtmlTestRunner
 
 
 url = 'https://www.tineye.com/'
 
 class UploadImagePage(unittest.TestCase):
 
-
+    @classmethod
     def setUp(self, browser="mozilla"):
         if browser == "chrome":
             self.driver = webdriver.Chrome(executable_path=r'../Drivers/ChromeDrive_75/chromedriver.exe')
@@ -22,6 +23,7 @@ class UploadImagePage(unittest.TestCase):
             raise Exception("Brak przeglądarki")
         return self.driver
 
+    @classmethod
     def tearDown(self):
         self.driver.close()
         self.driver.quit()
@@ -34,6 +36,8 @@ class UploadImagePage(unittest.TestCase):
         upload_image.send_keys('C:\\ReportingInHtmlTestRunner\\PageObjectModel\\image\\surfer-2335088_960_720.jpg')
         sleep(5)
 
+if __name__ == '__main__':
+     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:/ReportingInHtmlTestRunner/PageObjectModel/Reports'))
 
 
 
